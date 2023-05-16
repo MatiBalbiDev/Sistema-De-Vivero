d540d14269541a572f8c533ebf0317855f97b69e
MatiBalbiDev <matiasbalbi96@gmail.com>
2023-05-15 21:18:53 -0300

    nueva ubicacion de imagenes

--- js/listaFamilias.js
+++ js/listaFamilias.js
@@ -1,56 +1,53 @@
+function mostrarFamilia() {
+  const familias = [
+    {
+      nombre: "Lamiaceae",
+      imagen: "./assets/lamiaceae.jpg",
+    },
+    {
+      nombre: "Umbelíferas",
+      imagen: "./assets/umbeliferas.jfif",
+    },
+    {
+      nombre: "Flores",
+      imagen: "./assets/familia1.jpg",
+    },
+    {
+      nombre: "Árboles",
+      imagen: "./assets/familia1.jpg",
+    },
+    {
+      nombre: "Limoneros",
+      imagen: "./assets/familia1.jpg",
+    },
+    {
+      nombre: "Flores",
+      imagen: "./assets/familia1.jpg",
+    },
+  ];
 
-function mostrarFamilia(){
-    const familias = [
-        {
-            nombre: "Lamiaceae",
-            imagen: "./images/lamiaceae.jpg",
-        },
-        {
-            nombre: "Umbelíferas",
-            imagen: "./images/umbeliferas.jfif"
-        },
-        {
-            nombre: "Flores",
-            imagen: "./images/familia1.jpg",
-        },
-        {
-            nombre: "Árboles",
-            imagen: "./images/familia1.jpg",
-        },
-        {
-            nombre: "Limoneros",
-            imagen: "./images/familia1.jpg",
-        },
-        {
-            nombre: "Flores",
-            imagen: "./images/familia1.jpg",
-        },
-    ]
+  let contenedor = document.getElementById("cards");
 
-    let contenedor = document.getElementById("cards")
-
-    familias.map(
-        familia => {
-            contenedor.innerHTML+= `
+  familias.map((familia) => {
+    contenedor.innerHTML += `
             <div class="card" id="card" style="width: 18rem;">
                 <img src=${familia.imagen} class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${familia.nombre}</h5>
                     <p class="card-text">$40.000</p>
-                    <a onclick="${settearFamilia(familia.nombre)}" href="./listaPlantas.html"  class="btn btn-xs btn-dark btn-block rounded-0">VER PLANTAS</a>
+                    <a onclick="${settearFamilia(
+                      familia.nombre
+                    )}" href="./listaPlantas.html"  class="btn btn-xs btn-dark btn-block rounded-0">VER PLANTAS</a>
                 </div>
-            </div> `
-        }
-    )
-
-
+            </div> `;
+  });
 }
 
-function settearFamilia(nombreFamilia){
-    let familia = {
-        "nombre": "nombreFamilia"
-    }
-    localStorage.setItem("familia", JSON.stringify(familia))
+function settearFamilia(nombreFamilia) {
+  let familia = {
+    nombre: "nombreFamilia",
+  };
+  localStorage.setItem("familia", JSON.stringify(familia));
 }
 
-mostrarFamilia()
+mostrarFamilia();
--- js/listaPlantas.js
+++ js/listaPlantas.js
@@ -1,64 +1,63 @@
 const plantas = [
-    {
-        nombre: "Menta",
-        familia: "Lamiaceae",
-        fase: 1,
-        fila: 6,
-        columna: 9,
-        imagen: "./images/menta.jpg",
-        código: "#fk46p3"
-    },
-    {
-        nombre: "Romero",
-        familia: "Lamiaceae",
-        fase: 1,
-        fila: 5,
-        columna: 10,
-        imagen: "./images/romero.jpg",
-        código: "#fk46p3"
-    },
-    {
-        nombre: "Tomillo",
-        familia: "Lamiaceae",
-        fase: 1,
-        fila: 5,
-        columna: 10,
-        imagen: "./images/tomillo.jpg",
-        código: "#fk46p3"
-    },        
-    {
-        nombre: "Zanahoria",
-        familia: "Umbelíferas",
-        fase: 1,
-        fila: 5,
-        columna: 10,
-        imagen: "./images/zanahoria.jpg",
-        código: "#fk46p3"
-    },
-    {
-        nombre: "Alcaravea",
-        familia: "Umbelíferas",
-        fase: 1,
-        fila: 5,
-        columna: 10,
-        imagen: "./images/alcaravea.jpeg",
-        código: "#fk46p3"
-    },
-    {
-        nombre: "Chirivía",
-        familia: "Umbelíferas",
-        fase: 1,
-        fila: 5,
-        columna: 10,
-        imagen: "./images/chirivia.jpg",
-        código: "#fk46p3"
-    },       
-]
-let contenedor = document.getElementById("cards")
+  {
+    nombre: "Menta",
+    familia: "Lamiaceae",
+    fase: 1,
+    fila: 6,
+    columna: 9,
+    imagen: "./assets/menta.jpg",
+    código: "#fk46p3",
+  },
+  {
+    nombre: "Romero",
+    familia: "Lamiaceae",
+    fase: 1,
+    fila: 5,
+    columna: 10,
+    imagen: "./assets/romero.jpg",
+    código: "#fk46p3",
+  },
+  {
+    nombre: "Tomillo",
+    familia: "Lamiaceae",
+    fase: 1,
+    fila: 5,
+    columna: 10,
+    imagen: "./assets/tomillo.jpg",
+    código: "#fk46p3",
+  },
+  {
+    nombre: "Zanahoria",
+    familia: "Umbelíferas",
+    fase: 1,
+    fila: 5,
+    columna: 10,
+    imagen: "./assets/zanahoria.jpg",
+    código: "#fk46p3",
+  },
+  {
+    nombre: "Alcaravea",
+    familia: "Umbelíferas",
+    fase: 1,
+    fila: 5,
+    columna: 10,
+    imagen: "./assets/alcaravea.jpeg",
+    código: "#fk46p3",
+  },
+  {
+    nombre: "Chirivía",
+    familia: "Umbelíferas",
+    fase: 1,
+    fila: 5,
+    columna: 10,
+    imagen: "./assets/chirivia.jpg",
+    código: "#fk46p3",
+  },
+];
+let contenedor = document.getElementById("cards");
 
-    plantas.map(
-        planta => {
-            contenedor.innerHTML+= ` 
+plantas.map((planta) => {
+  contenedor.innerHTML += ` 
             <div class="card" id="card" style="width: 18rem;">
                 <img src=${planta.imagen} class="card-img-top" alt="...">
                 <div class="card-body">
@@ -69,19 +68,17 @@ let contenedor = document.getElementById("cards")
                     
                     <a href=#  class="btn btn-xs btn-dark btn-block rounded-0">AGREGAR AL CARRITO</a>
                 </div>
-            </div> `
-        }
-    )
-function cualquierCosa(){
-    var familia = JSON.parse(localStorage.getItem("familia"))
+            </div> `;
+});
+function cualquierCosa() {
+  var familia = JSON.parse(localStorage.getItem("familia"));
 
-    let container = document.getElementById("cards")
+  let container = document.getElementById("cards");
 
-    plantas.map( planta => {
-        if(planta.familia == familia.nombre){
-            container.innerHTML += planta.nombre
-        }
-    })
+  plantas.map((planta) => {
+    if (planta.familia == familia.nombre) {
+      container.innerHTML += planta.nombre;
+    }
+  });
 }
-cualquierCosa()
-
+cualquierCosa();
